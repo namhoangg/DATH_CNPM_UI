1c18f3847ca646958e229348520ccfec08f9e3cb
Vu Luong <luongvu151003@gmail.com>
2023-12-31 21:17:23 +0700

    3rd

--- website/views.py
+++ website/views.py
@@ -7,22 +7,6 @@ from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        phone_number = request.form.get('phone_number')
-        password = request.form.get('password')
-
-        buyer = Buyer.query.filter_by(Phone_number = phone_number).first()
-
-        if buyer:
-            if buyer.Password == password:
-                #login_user(buyer, remember=True)
-                print('successfully logged in')
-                return redirect(url_for('auth.login'))
-            else:
-                return redirect(url_for('auth.login'))
-        else:
-            return redirect(url_for('auth.login'))
-        
-    return render_template("login.html")
+@views.route('/')
+def home(): 
+    return render_template('webcart.html')
