40242906bf1f094a23c0a492efaad60d8f67f733
Vu Luong <luongvu151003@gmail.com>
2024-01-03 01:43:43 +0700

    .

--- website/model.py
+++ website/model.py
@@ -1,7 +1,7 @@
 from . import db
 from sqlalchemy.sql import func
 from flask_login import UserMixin
-from sqlalchemy.dialects.mysql import CHAR, VARCHAR, DATE
+from sqlalchemy.dialects.mysql import CHAR, VARCHAR, DATE, DECIMAL, TEXT, INTEGER
 
 class Buyer(db.Model, UserMixin):
     __tablename__ = 'buyer'
@@ -22,3 +22,31 @@ class Buyer(db.Model, UserMixin):
 
     def __repr__(self):
         return f'<Buyer {self.First_Name} {self.Last_Name}>'
+    
+class Product(db.Model):
+    __tablename__ = 'product'
+
+    code = db.Column(CHAR(10), primary_key=True)
+    name = db.Column(VARCHAR(255), nullable=False)
+    category = db.Column(VARCHAR(255), nullable=False)
+    price = db.Column(DECIMAL(10, 2), nullable=False)
+    quantity = db.Column(INTEGER, nullable=False)
+    description = db.Column(TEXT, nullable=False)
+    discount = db.Column(INTEGER, nullable=False)
+    brand = db.Column(VARCHAR(255), nullable=False)
+    review_rating = db.Column(DECIMAL(2, 1))
+    review_count = db.Column(INTEGER, default=0)
+    sold_count = db.Column(INTEGER, default=0)
+    image_url = db.Column(VARCHAR(255))
+
+    __table_args__ = (
+        db.CheckConstraint('price > 0', name='check_price'),
+        db.CheckConstraint('quantity >= 0', name='check_quantity'),
+        db.CheckConstraint('discount >= 0 AND discount <= 99', name='check_discount'),
+        db.CheckConstraint('review_rating >= 0 AND review_rating <= 5', name='check_review_rating'),
+        db.CheckConstraint('review_count >= 0', name='check_review_count'),
+        db.CheckConstraint('sold_count >= 0', name='check_sold_count'),
+    )
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
--- website/views.py
+++ website/views.py
@@ -2,12 +2,62 @@
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user
-from .model import Buyer
+from sqlalchemy import or_
+from .model import Buyer, Product
 from . import db
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home(): 
-    return render_template('homepage.html')
+    top_discounted_products = Product.query.order_by(Product.discount.desc()).limit(5).all()
+    return render_template('homepage.html', products=top_discounted_products)
 
+@views.route('/list-view')
+def list_view():
+    products = Product.query.all()  # Fetches all products
+    return render_template('list-view.html', products=products)
+
+@views.route('/search-results')
+def search_results():
+    keyword = request.args.get('keyword', '')
+    category = request.args.get('category', 'all')
+
+    query = Product.query
+
+    if keyword:
+        query = query.filter(Product.name.ilike(f'%{keyword}%'))  # Adjust the Product.name to your model
+
+    if category != 'all':
+        query = query.filter_by(category=category)  # Adjust the Product.category to your model
+
+    products = query.all()
+    return render_template('list-view.html', products=products)
+
+@views.route('/apply-filters')
+def apply_filters():
+    selected_brands = request.args.getlist('brand')
+    min_price = request.args.get('min_price', type=float, default=0.0)
+    max_price = request.args.get('max_price', type=float, default=float('inf'))
+    selected_ratings = request.args.getlist('rating', type=int)
+
+    query = Product.query
+
+    if selected_brands:
+        query = query.filter(Product.brand.in_(selected_brands))
+    
+    query = query.filter(Product.price >= min_price, Product.price <= max_price)
+  
+    if selected_ratings:
+        rating_filters = []
+        for rating in selected_ratings:
+            if rating == 5:
+                rating_filters.append(Product.review_rating >= 5)
+            else:
+                rating_filters.append((Product.review_rating >= rating) & (Product.review_rating < rating + 1))
+        
+        if rating_filters:
+            query = query.filter(or_(*rating_filters))
+
+    products = query.all()
+    return render_template('list-view.html', products=products)
